d9ffcfe27928c03772a03bb79a7cf58962e30d2b
GaryGolf <garygolf@mail.ru>
2018-12-09 17:14:28 +0300

    update app component

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react';
-import db from './api';
+import api from './api';
+import { PopularDataResult } from './api';
+import Movies from './Components/Movie'
+
 interface Props {
 
 }
-export default class App extends React.PureComponent<Props, {}> {
+interface State {
+  movies: PopularDataResult[];
+}
+export default class App extends React.PureComponent<Props, State> {
+
+  constructor(props:Props) {
+    super(props);
+    this.state = { movies: []};
+  }
   
   componentDidMount(){
-    db.getPopular()
-    .then(console.log)
+    api.getPopular().then(result => this.setState({ movies: result.data.results }));
   }
+  
   render(){
-    return <h2>Hello MOvie db</h2>
+    return (
+      <div>
+        <h2>Hello MOvie db</h2>
+        <Movies.List movies={this.state.movies}/>
+      </div>
+    )
   }
 }
