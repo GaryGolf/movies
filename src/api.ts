4530ea405245930bbcd9f637b89092f42deec95a
GaryGolf <garygolf@mail.ru>
2018-12-09 17:13:44 +0300

    add popular data api interface

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,36 @@
-import axios from 'axios';
+import Axios, { AxiosPromise, AxiosResponse } from 'axios';
 import { API_KEY } from './constants';
 
+
+export interface PopularDataResult {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface PopularData {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: PopularDataResult[];
+
+}
+
+
 export default {
-  getPopular: async (page = 1) => {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
-    return axios.get(url)
-      .then(result => result)
-  }
+  
+  getPopular: (page = 1):AxiosPromise<PopularData> => Axios
+    .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`)
+    .then((result:AxiosResponse<PopularData>) => result)
 }
