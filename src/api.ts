caa4d57945366143dadb69b88c4a608047ed91c7
GaryGolf <garygolf@mail.ru>
2018-12-15 22:24:59 +0300

    add movie details interface

--- src/api.ts
+++ src/api.ts
@@ -27,10 +27,41 @@ export interface PopularData {
 
 }
 
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {id: number, name: string, poster_path: string, backdrop_path: string };
+  budget: number;
+  genres: Array<{ id: number, name: string}>
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: Array<{ id: number, logo_path: string, name: string, origin_country: string }>;
+  production_countries: Array<{iso_3166_1: string, name: string }>;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Array<{ iso_639_1: string, name: string }>;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 export default {
   
   getPopular: (page = 1):AxiosPromise<PopularData> => Axios
     .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`)
-    .then((result:AxiosResponse<PopularData>) => result)
+    .then((result:AxiosResponse<PopularData>) => result),
+
+  getDetails: (movie_id: string) => Axios
+    .get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
+    .then((result:AxiosResponse<MovieDetails>) => result),
 }
