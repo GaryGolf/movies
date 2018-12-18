967963319a514b659d394de70db37e6e15bc46ac
GaryGolf <garygolf@mail.ru>
2018-12-18 09:39:28 +0300

    minor changes

--- src/store/movie-details/actions.ts
+++ src/store/movie-details/actions.ts
@@ -3,9 +3,10 @@ import { getDetails } from './api';
 export const CLEAR_MOVIE_DETAILS = 'CLEAR_MOVIE_DETAILS';
 
 export const FETCH_MOVIE_DETAILS = 'FETCH_MOVIE_DETAILS';
-export const fetchMovieDetails = (id:string) => dispatch => {
-  dispatch({ type: CLEAR_MOVIE_DETAILS });
-  getDetails(id).then(result => {
-    dispatch({ type: FETCH_MOVIE_DETAILS, payload: result.data })
-  });
-}
+export const fetchMovieDetails = (id:string) => 
+  dispatch => {
+    dispatch({ type: CLEAR_MOVIE_DETAILS });
+    getDetails(id).then(result => {
+      dispatch({ type: FETCH_MOVIE_DETAILS, payload: result.data })
+    });
+  };
