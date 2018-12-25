f90cfedc53bb97f9f624299975d22f5aedea932b
GaryGolf <garygolf@mail.ru>
2018-12-25 22:17:18 +0300

    try to get poster img

--- src/components/movies/list-item.tsx
+++ src/components/movies/list-item.tsx
@@ -13,7 +13,12 @@ export class ListItem extends React.PureComponent<Props, {}> {
   render() {
     return (
       <Link className={styles.listLink} to={`/details/${this.props.item.id}`}>
-        <li className={styles.listItem}>{this.props.item.title}</li>
+        <li className={styles.listItem}>
+          <h3>
+            {this.props.item.title}
+          </h3>
+          <img src={'https://api.themoviedb.org/3/movie' + this.props.item.poster_path}/>
+        </li>
       </Link>
     )
   }
