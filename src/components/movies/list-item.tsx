08bbbc1afe9d376c7720dc5c8efe07b861926990
GaryGolf <garygolf@mail.ru>
2018-12-26 22:04:54 +0300

    add poster path

--- src/components/movies/list-item.tsx
+++ src/components/movies/list-item.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { PopularMovie } from 'store/popular-movies/definition';
-
 import * as styles from './list-item.scss'
 
 interface Props {
@@ -10,14 +9,15 @@ interface Props {
 
 export class ListItem extends React.PureComponent<Props, {}> {
 
+
+
   render() {
+    const { id, title, poster_path } = this.props.item;
     return (
-      <Link className={styles.listLink} to={`/details/${this.props.item.id}`}>
+      <Link className={styles.listLink} to={`/details/${id}`}>
         <li className={styles.listItem}>
-          <h3>
-            {this.props.item.title}
-          </h3>
-          <img src={'https://api.themoviedb.org/3/movie' + this.props.item.poster_path}/>
+          <h3> {title} </h3>
+          <img src={'https://image.tmdb.org/t/p/w185/' + poster_path}/>
         </li>
       </Link>
     )
